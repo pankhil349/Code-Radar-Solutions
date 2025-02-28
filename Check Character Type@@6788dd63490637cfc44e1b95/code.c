fcd35c2542cfeadfb486e7650fd1a21e5cc1eb3b
pankhil349 <kapoorpankhil34@gmail.com>
2025-02-28 22:41:50 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,19 +1,26 @@
-// Your code here...
-#include<stdio.h>
-int main(){
-    char a;
-    scanf("%c", &a);
-    if ( a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U'){
-        printf("Vowel");
-    }
-    else if ((a >= 'a' && a <= 'z') || (a <= 'A' && a<= 'Z')){
-        printf("Consonant");
-    }
-    else if ( a >= '0' && a<='9'){
+#include <stdio.h>
+#include <ctype.h>
+
+void checkCharacter(char ch) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) { // Check if it is a letter
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            printf("Vowel");
+        } else {
+            printf("Consonant");
+        }
+    } else if (ch >= '0' && ch <= '9') { // Check if it is a digit
         printf("Digit");
-    }
-    else{
+    } else { // If it's neither a letter nor a digit, it's a special character
         printf("Special Character");
     }
+}
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+    
+    checkCharacter(ch);
+    
     return 0;
 }
