bc5cd1445ac32faa2d533bf479d68a0daf5f0e00
pankhil349 <kapoorpankhil34@gmail.com>
2025-04-11 15:13:06 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -20,7 +20,7 @@ int main() {
             break;
         case '/':
             if (b == 0) {
-                printf("Error: Division by zero.\n");
+                printf("error\n");
             } else {
                 printf("%d\n", a / b);
             }
